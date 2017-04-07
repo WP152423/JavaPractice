310edb335ae3696568b8c95eca6f311be62c3fa9
Leejaeho <Leejaeho@218.38.137.28>
2017-04-07 15:44:31 +0900

    실습과제 snack

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_Snack
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 6.
+ * <pre>
+ * @author : Leejaeho
+ * @version : 1.0
+ */
+public class Snack {
+		private String name;
+		private String company;
+		private int price;
+		private int number;
+		
+		public String toString(){
+			return "이름 : " + name + "\n" + "제조사 : " +company+"\n"+ "가격 : " +String.format("%,d", price) + "원"+"\n"+ "개수 : "+ number + "개"+"\n";
+			
+			
+			
+		}
+		
+		public int calcPrice()
+		{
+			return price * number;
+		}
+		
+		public Snack(){
+			
+		}
+		
+		public Snack(String name, String company, int price, int number)
+		{
+			this.name = name;
+			this.company = company;
+			this.price = price;
+			this.number  = number;
+		}
+		
+		
+	   public String getName(){
+	      return name;
+	   }
+	   
+	   public String getCompany(){
+	      return company;
+	   }
+	   
+	   public int getPrice(){
+	      return price;
+	   }
+	   
+	   public int getNumber(){
+	      return number;
+	   }
+	   
+	   
+	   
+	   public void setName(String newName){
+	      name = newName;
+	      
+	   }
+
+	   public void setCompany(String newCompany){
+	      company = newCompany;
+	   }
+	   
+	   public void setPrice(int newPrice){
+	      price = newPrice;
+	   }
+	   
+	   public void setNumber(int newNumber){
+	     number = newNumber;
+	   }
+	   
+	   
+}
